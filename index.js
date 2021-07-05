925760c866370a5591984b8033d69183faacd85e
trog <joe@rconjoe.com>
2021-07-05 06:29:25 +0000

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const app = require('express')()
+const httpServer = require('http').createServer(app)
+const options = { cors: { origin: "*" } }
+const io = require('socket.io')(httpServer, options)
+const pty = require('node-pty')
+
+io.on('connection', socket => {
+    console.log('user connected')
+
+    const term = pty.spawn('bash', [], {
+        name: 'xterm-color',
+        cols: 80,
+        rows: 35,
+        cwd: process.env.HOME,
+        env: process.env
+    })
+    term.on('data', data => {
+        socket.emit('message', data)
+    })
+    socket.on('message', data => {
+        term.write(data)
+        console.log('message sent')
+    })
+})
+app.get('/test', (req, res) => {
+    console.log('it works')
+    res.send('gameserver api test endpoint')
+})
+httpServer.listen(3000)
